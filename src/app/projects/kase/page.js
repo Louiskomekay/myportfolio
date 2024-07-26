84be5b5a6dfc30a0c7890b7aa95a40186e2f23d2
Louiskome <okwoho.kome@gmail.com>
2024-07-27 00:27:06 +0100

    initial setup

---
+++ src/app/projects/kase/page.js
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>kase project page</div>
+    )
+}
+
+export default page
---
+++ src/app/projects/zeel/page.js
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>zeel project page</div>
+    )
+}
+
+export default page
