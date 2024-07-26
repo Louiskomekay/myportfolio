4851276a3e6007f1ae5f3af22b4f48034782bc5d
Louiskome <okwoho.kome@gmail.com>
2024-07-26 23:52:56 +0100

    First commit files

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      backgroundImage: {
+        'aboutImgBg': "url('https://res.cloudinary.com/dgewamfyi/image/upload/v1721499528/My%20portfolio/About_BG_img_yz9ree.jpg')"
+      }
+    }
+  },
+  plugins: [],
+};
